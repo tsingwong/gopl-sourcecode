cb42cadd4b1adf3e4d31f9cd8f6eb8ec76cba8ba
tsingwong <919916459@qq.com>
2021-05-10 08:02:52 +0800

    feat: add 1.3 ex

--- ch01/06-dup2/main.go
+++ ch01/06-dup2/main.go
@@ -3,7 +3,7 @@
  * @Author: Tsingwong
  * @Date: 2021-05-08 18:30:07
  * @LastEditors: Tsingwong
- * @LastEditTime: 2021-05-08 19:21:59
+ * @LastEditTime: 2021-05-09 00:06:58
  */
 package main
 
@@ -13,11 +13,16 @@ import (
 	"os"
 )
 
+type obj struct {
+	filename string
+	count    int
+}
+
 func main() {
-	counts := make(map[string]int)
+	counts := make(map[string]*obj)
 	files := os.Args[1:]
 	if len(files) == 0 {
-		countLines(os.Stdin, counts)
+		countLines(os.Stdin, counts, "none")
 	} else {
 		for _, arg := range files {
 			f, err := os.Open(arg)
@@ -26,20 +31,29 @@ func main() {
 				fmt.Fprintf(os.Stderr, "dup2:%v\n", err)
 				continue
 			}
-			countLines(f, counts)
+			countLines(f, counts, arg)
 			f.Close()
 		}
 	}
 	for line, n := range counts {
-		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
+		if n.count > 1 {
+			fmt.Printf("%d\t %s\t%s\n", n.count, n.filename, line)
 		}
 	}
 }
 
-func countLines(f *os.File, counts map[string]int) {
+func countLines(f *os.File, counts map[string]*obj, filename string) {
 	input := bufio.NewScanner(f)
+	fmt.Println(counts)
 	for input.Scan() {
-		counts[input.Text()]++
+		if _, ok := counts[input.Text()]; !ok {
+			counts[input.Text()] = &obj{
+				filename: filename,
+				count:    1,
+			}
+		} else {
+			counts[input.Text()].count++
+		}
+
 	}
 }
