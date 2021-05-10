4db3624a77ca5320f19e0e99a8192578c7175df3
tsingwong <tsingwoong@163.com>
2021-05-11 07:58:57 +0800

    feat: add ch01-07 server2

---
+++ ch01/11-server2/main.go
@@ -0,0 +1,37 @@
+/*
+ * @Description:
+ * @Author: Tsingwong
+ * @Date: 2021-05-10 23:51:29
+ * @LastEditors: Tsingwong
+ * @LastEditTime: 2021-05-11 07:58:18
+ */
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+)
+
+var mu sync.Mutex
+var count int
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/count", counter)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	count++
+	mu.Unlock()
+	fmt.Fprintf(w, "URL.path = %q\n", r.URL.Path)
+}
+
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "Count %d\n", count)
+	mu.Unlock()
+}
