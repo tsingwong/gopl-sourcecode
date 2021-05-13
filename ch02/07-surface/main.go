be421febf1a5cbff95e24d47258a226c72d078dd
tsingwong <tsingwoong@163.com>
2021-05-13 23:14:38 +0800

    feat: add ex 3.4

--- ch02/07-surface/main.go
+++ ch02/07-surface/main.go
@@ -3,13 +3,17 @@
  * @Author: Tsingwong
  * @Date: 2021-05-13 22:48:46
  * @LastEditors: Tsingwong
- * @LastEditTime: 2021-05-13 22:58:11
+ * @LastEditTime: 2021-05-13 23:14:17
  */
 package main
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"math"
+	"net/http"
+	"os"
 )
 
 const (
@@ -24,7 +28,22 @@ const (
 var sin30, cos30 = math.Sin(angle), math.Cos(angle)
 
 func main() {
-	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
+	if len(os.Args) > 1 && os.Args[1] == "web" {
+		handler := func(w http.ResponseWriter, r *http.Request) {
+			// 设置 Content-Type 用于标识返回结果是 svg 的图片
+			w.Header().Set("Content-Type", "image/svg+xml")
+			genSvg(w)
+		}
+		http.HandleFunc("/", handler)
+		log.Fatal(http.ListenAndServe("localhost:8000", nil))
+		return
+	}
+	genSvg(os.Stdout)
+}
+
+func genSvg(out io.Writer) {
+	// 使用 Fprintf 向 这个实现了 io.Writer 的接口里输出
+	fmt.Fprintf(out, "<svg xmlns='http://www.w3.org/2000/svg' "+
 		"style='stroke: grey; fill: white; stroke-width: 0.7' "+
 		"width='%d' height='%d'>", width, height)
 	for i := 0; i < cells; i++ {
@@ -33,11 +52,15 @@ func main() {
 			bx, by := corner(i, j)
 			cx, cy := corner(i, j+1)
 			dx, dy := corner(i+1, j+1)
-			fmt.Printf("<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
+			if math.IsNaN(ax) || math.IsNaN(ay) || math.IsNaN(bx) || math.IsNaN(by) || math.IsNaN(cx) || math.IsNaN(cy) || math.IsNaN(dx) || math.IsNaN(dy) {
+				continue
+			}
+
+			fmt.Fprintf(out, "<polygon points='%g,%g %g,%g %g,%g %g,%g'/>\n",
 				ax, ay, bx, by, cx, cy, dx, dy)
 		}
 	}
-	fmt.Println("</svg>")
+	fmt.Fprintf(out, "</svg>")
 }
 
 // 返回每个网格顶点的坐标
