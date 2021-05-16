9e152033340e68ce38d215be58148cb53aa61516
tsingwong <tsingwoong@163.com>
2021-05-17 07:31:08 +0800

    feat: add ex 3.14

---
+++ ch03/04-kb2yb/main.go
@@ -0,0 +1,20 @@
+/*
+ * @Description:
+ * @Author: Tsingwong
+ * @Date: 2021-05-17 07:11:45
+ * @LastEditors: Tsingwong
+ * @LastEditTime: 2021-05-17 07:19:16
+ */
+package main
+
+const (
+	_ = 1 << (10 * iota)
+	KB
+	MB
+	GB
+	TB
+	PB
+	EB
+	ZB
+	YB
+)
